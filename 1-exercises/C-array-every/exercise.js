9c742a4be5d9b9262ece91c9315df55c085b1c07
theTrainMan <stormbreaker70@gmail.com>
2021-02-02 19:55:32 +0200

    C-array-every

--- 1-exercises/C-array-every/exercise.js
+++ 1-exercises/C-array-every/exercise.js
@@ -5,13 +5,21 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+var groupIsOnlyStudents = students.every(isStudent); // complete this statement
 
-if (groupIsOnlyStudents) {
-  console.log("The group contains only students");
-} else {
-  console.log("The group does not contain only students");
+function isStudent(item) {
+    if (groupIsOnlyStudents) {
+        console.log("The group contains only students");
+    } else {
+        console.log("The group does not contain only students");
+    }
 }
+
+// if (groupIsOnlyStudents) {
+//     console.log("The group contains only students");
+// } else {
+//     console.log("The group does not contain only students");
+// }
 
 /* EXPECTED RESULT */
 
