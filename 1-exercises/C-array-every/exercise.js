7d458b87b22f2b96222313dba375213370cafb19
theTrainMan <stormbreaker70@gmail.com>
2021-02-13 10:28:18 +0200

    C-array-Every- Redone

--- 1-exercises/C-array-every/exercise.js
+++ 1-exercises/C-array-every/exercise.js
@@ -5,9 +5,32 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents = group.every(function(students, group) {
-    return students == group;
-}); // complete this statement
+let isStudents = (item) => {
+    console.log(item);
+    return students.includes(item)
+
+}
+var groupIsOnlyStudents = group.every(isStudents);
+
+if (groupIsOnlyStudents) {
+    console.log("The group contains only students");
+} else {
+    console.log("The group does not contain only students");
+}
+
+// var groupIsOnlyStudents = group.every(function(item, index) {
+//     console.log(item + " at " + index);
+//     return true;
+//     // return  students == group;
+// }); // complete this statement
+// var groupIsOnlyStudents = students.every(function(item, index) {
+//     console.log(item + " at " + index);
+//     return true;
+//     // return  students == group;
+// }); // complete this statement
+
+
+
 
 // function isStudent(item) {
 //     if (groupIsOnlyStudents) {
@@ -16,31 +39,20 @@ var groupIsOnlyStudents = group.every(function(students, group) {
 //         console.log("The group does not contain only students");
 //     }
 // }
-for (let i = 0; i <= group.length; i++) {
-    let person = group[i];
-    console.log('Checking' + person);
-    let personIsStudent = false;
-    for (let j = 0; j <= students.length; j++) {
-        if (person === students[j]) {
-            personIsStudent === true;
-            break;
-        };
-    }
-}
-
-if (groupIsOnlyStudents) {
-    console.log("The group contains only students");
-} else {
-    console.log("The group does not contain only students");
-}
-
-
-
-// if (groupIsOnlyStudents) {
-//     console.log("The group contains only students");
-// } else {
-//     console.log("The group does not contain only students");
+// for (let i = 0; i <= group.length; i++) {
+//     let person = group[i];
+//     console.log('Checking' + person);
+//     let personIsStudent = false;
+//     for (let j = 0; j <= students.length; j++) {
+//         console.log('Checking student ' + students[j]);
+//         if (person === students[j]) {
+//             personIsStudent === true;
+//             break;
+//         };
+//     }
 // }
+
+
 
 /* EXPECTED RESULT */
 
